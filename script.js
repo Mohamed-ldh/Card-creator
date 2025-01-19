b77d32d372a13172e3630554e486ed6d5f885882
med.lbdh <62430492+Mohamed-ldh@users.noreply.github.com>
2025-01-19 17:31:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let n=document.getElementById('name');
 let img=document.getElementById('image');
 let c=document.getElementById('color');
 let list=document.getElementById('list');
-let al = document.getElementById('alertMessage');
 
 function ajout(){
     if(realTimeVerifName()==true && realTimeVerifImg()==true){
@@ -45,42 +44,30 @@ function updateList(){
 }
 
 function realTimeVerifName(){
-    let v=document.getElementById('name').value;
-    if(v.length == 0){
-        document.getElementById('name').style.border = "4px solid red";
-        document.getElementById('name').style.color='red';
+    let v=document.getElementById('name');
+    if(v.value.length == 0){
+        v.style.border = "4px solid red";
+        v.style.color='red';
         return true;
     }
     else{
-        document.getElementById('name').style.border = "4px solid green";
-        document.getElementById('name').style.color='green';
+        v.style.border = "4px solid green";
+        v.style.color='green';
         return false;
     }
 }
 
 
 function realTimeVerifImg(){
-    let v=document.getElementById('image').value;
-    if(v.length == 0){
-        document.getElementById('image').style.border = "4px solid red";
-        document.getElementById('image').style.color='red';
+    let v=document.getElementById('image');
+    if(v.value.length == 0){
+        v.style.border = "4px solid red";
+        v.style.color='red';
         return true;
     }
     else{
-        document.getElementById('image').style.border = "4px solid green";
-        document.getElementById('image').style.color='green';
+        v.style.border = "4px solid green";
+        v.style.color='green';
         return false;
-    }
-}
-
-function realTimeVerif(){
-    var v=inp.value;
-    if(v.length < 3){
-        inp.style.border = "4px solid red";
-        inp.style.color='red';
-    }
-    else{
-        inp.style.border = "4px solid green";
-        inp.style.color='green';
     }
 }
